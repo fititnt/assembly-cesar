acdc7cd05a5b0ae1acaa6e3df43dbe071f3d0fd4
Emerson Rocha Luiz <emerson@alligo.com.br>
2014-10-05 21:23:21 +0000

    iniciado

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fititnt
+ *
+ * Created on 5 de Outubro de 2014, 21:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
